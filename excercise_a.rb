5ce142989b02fd2608a9f690a5d77797b74df22f
Joe Stafford <joestafford1986@gmail.com>
2018-04-11 22:18:02 +0100

    added tasks 2 + 3

--- excercise_a.rb
+++ excercise_a.rb
@@ -1,9 +1,19 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 #excercise methods
+def add_to_end(station_name, station_array)
+  station_array.push(station_name)
+  p station_array
+end
+
 def add_to_start(station_name, station_array)
   station_array.unshift(station_name)
   p station_array
+end
+
+def add_at_position(station_name, position, array)
+  array.insert(position, station_name)
+  p array
 end
 
 
@@ -13,4 +23,6 @@ end
 
 #--------------------------------------------
 #invoke methods
-add_to_start("Edinburgh Waverley", stops)
+add_to_end("Edinburgh Waverley", stops)
+add_to_start("Glasgow Queen St", stops)
+add_at_position("Polmont", 4, stops)
